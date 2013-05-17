bd037996d95f8f6efcdcf266aa110a577749ecdc
eoptam <eoptam@gmail.com>
2013-05-17 11:09:27 +0200

    clean bat

--- #clean.bat
+++ #clean.bat
@@ -1,6 +1,7 @@
 del /s *.cachefile
 rd /q /s ManipAnalysis\bin
 rd /q /s ManipAnalysis\obj
+rd /q /s ManipAnalysis\Deploy
 rd /q /s "ManipAnalysis\Service References"
 rd /q /s ManipAnalysisLib\bin
 rd /q /s ManipAnalysisLib\obj
