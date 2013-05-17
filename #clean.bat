5aa3e1058cbbc352053d4f1303d00dd2b5828607
eoptam <eoptam@gmail.com>
2013-05-17 11:07:42 +0200

    deleted stuff

--- #clean.bat
+++ #clean.bat
@@ -2,3 +2,6 @@ del /s *.cachefile
 rd /q /s ManipAnalysis\bin
 rd /q /s ManipAnalysis\obj
 rd /q /s "ManipAnalysis\Service References"
+rd /q /s ManipAnalysisLib\bin
+rd /q /s ManipAnalysisLib\obj
+rd /q /s "ManipAnalysisLib\Service References"
