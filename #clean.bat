a2ef4cd297c47169f2dc089931327739691e3142
eoptam <eoptam@gmail.com>
2013-05-21 09:50:51 +0200

    New clean bat

--- #clean.bat
+++ #clean.bat
@@ -5,6 +5,3 @@ rd /q /s ManipAnalysis\bin
 rd /q /s ManipAnalysis\obj
 rd /q /s ManipAnalysis\Deploy
 rd /q /s "ManipAnalysis\Service References"
-rd /q /s ManipAnalysisLib\bin
-rd /q /s ManipAnalysisLib\obj
-rd /q /s "ManipAnalysisLib\Service References"
