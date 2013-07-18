2a911ebf1c2dd839d88b16119465984288ee844a
eoptam <eoptam@gmail.com>
2013-07-18 13:44:48 +0200

    commented m-file

--- ManipAnalysis/MatlabFiles/normVectorLine.m
+++ ManipAnalysis/MatlabFiles/normVectorLine.m
@@ -7,34 +7,42 @@
 %   Matthias Pöschl                                                     %
 %   Christian Stockinger, christian.stockinger@kit.edu                  %
 %                                                                       %
-%   12.05.2012                                                          %
+%   17.07.2013                                                          %
 %=======================================================================%
 
 %   normVectorLine.m for ManipAnalysis
 %   
 %   Description                                                     
-%   This function rotates a force vektor so it alignes with a normal vektor
-%   of a line segment.
+%   This function receives a line segment and a force vector for this segment.
+%   Then, the orthogonal force vector for this line segment ist calculated.
 %
 %   Arguments
 %   - Input:
-%       Vpos1 = Vector of dimension N (=2) representating x and z-coordinates
-%       of trajectory point 1 (first)
-%       Vpos2 = Vector of dimension N (=2) representating x and z-coordinates
-%       of trajectory point 2 (second)
-%       Vforce = Vector of dimension N (=2) representating z-coordinates
-%       of trajectory points
+%       Vpos1 = Start vector of the line segment
+%       
+%       Vpos2 = End vector of the line segment
+%       
+%       Vforce = Force vector for the given line segment
+%
 %   - Output:
-%       VforcePD = length of the 2dim trajectory
+%       VforcePD = Orthogonal force vector for the line segment
 
 function [VforcePD] = normVectorLine(Vpos1, Vpos2, Vforce)
 
+% Calculate the absolute force of the force vector
 AbsForce = sqrt(Vforce(1)^2 + Vforce(2)^2);
 
+% Calculate the direction vector ofd the line segment
 Vpos = (Vpos2 - Vpos1);
 
+% Calculate the angle between the direction vector and the force vector
 AngleBetweenVectors = atan2(Vforce(2), Vforce(1)) - atan2(Vpos(2), Vpos(1));
 
+% Calculate the orthogonal force at the direction vector
 AbsForceOrtho = AbsForce * sin(AngleBetweenVectors);
 
-VforcePD = ([-Vpos(2) Vpos(1)] / sqrt(Vpos(1)^2 + Vpos(2)^2)) * AbsForceOrtho;
+% Calculate the orthogonal direction vector
+VposPD = [-Vpos(2) Vpos(1)];
+
+% Normalise orthogonal direction vector and resize it with the absolute orthogonal force
+VforcePD = (VposPD / sqrt(Vpos(1)^2 + Vpos(2)^2)) * AbsForceOrtho;
