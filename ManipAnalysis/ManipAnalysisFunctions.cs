0ae43d07d1e958b99f409417cadf202fcb4c413f
eoptam <eoptam@gmail.com>
2015-11-11 11:48:26 +0100

    fix for study8 baseline claculation

--- ManipAnalysis/ManipAnalysisFunctions.cs
+++ ManipAnalysis/ManipAnalysisFunctions.cs
@@ -2557,7 +2557,7 @@ namespace ManipAnalysis_v2
                                                 baseline = baselineBuffer.Find(t => t.Study == trial.Study && t.Group == trial.Group && t.Subject == trial.Subject && t.Target.Number == trial.Target.Number && t.TrialType == trial.TrialType && t.ForceFieldType == Trial.ForceFieldTypeEnum.NullField && t.Handedness == trial.Handedness);
                                                 if (baseline == null)
                                                 {
-                                                    baseline = _myDatabaseWrapper.GetBaseline(trial.Study, trial.Group, trial.Subject, trial.Target.Number, Trial.TrialTypeEnum.StandardTrial, Trial.ForceFieldTypeEnum.NullField, trial.Handedness, baselineFields);
+                                                    baseline = _myDatabaseWrapper.GetBaseline(trial.Study, trial.Group, trial.Subject, trial.Target.Number, trial.TrialType, Trial.ForceFieldTypeEnum.NullField, trial.Handedness, baselineFields);
                                                     if (baseline != null)
                                                     {
                                                         lock (baselineBuffer)
