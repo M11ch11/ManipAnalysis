81369ba557d54433ed865c062d1171c3872a2b9d
Alexander Wolpert <Alexander.wolpert@googlemail.com>
2016-07-18 19:21:23 +0200

    Changed Deploy to new GitHubRepo and changed gh-pages to properly link new GitHubRepo

---
+++ ManipAnalysis/bin/Debug/MatlabFiles/fisherZVectorCorrelationTransform.m
@@ -0,0 +1,28 @@
+%=======================================================================%   
+%   Karlsruhe Institute of Technology (KIT)                             %
+%   Department of Sport and Sport Science                               %
+%   BioMotion Center                                                    %
+%   www.sport.kit.edu                                                   %
+%                                                                       %
+%   Matthias Pöschl                                                     %
+%   Christian Stockinger, christian.stockinger@kit.edu                  %
+%                                                                       %
+%   07.11.2013                                                          %
+%=======================================================================%
+
+%   fisherZVectorCorrelationTransform.m for ManipAnalysis
+%   
+%   Description                                                     
+%   This function transforms Fisher-Z-Values to vector correlation coeffcient
+%	r-Values using backward Fisher Z-transformation.
+%
+%   Arguments
+%   - Input:
+%       fisherZ = Fisher Z-transformed velocity vector correlation
+%       coefficient (z-values)
+%   - Output:
+%       corr = velocity vector correlation coefficient (r-values)
+
+function [corr] = fisherZVectorCorrelationTransform(fisherZ)
+
+corr = (exp(2*fisherZ) - 1) ./ (exp(2*fisherZ) + 1); % Backward transformation of Fisher Z-transform to r-values
