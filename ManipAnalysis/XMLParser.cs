fd5207e7666c5d65595de8272d4cb07dd6bcd466
Alx-Wo <undzl@student.kit.edu>
2017-05-14 13:14:34 +0200

    working on the xml/dtp parser

---
+++ ManipAnalysis/XMLParser.cs
@@ -0,0 +1,37 @@
+﻿using ManipAnalysis_v2.MongoDb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ManipAnalysis_v2
+{
+    class XMLParser
+    {
+        XmlDocument document;
+        Trial trial;
+
+        public XMLParser (string path, int trialNumber)
+        {
+            //Assertion: The Trial_Num in the c3d file can be identified and it actually represents the TrialNumberInSzenario
+            document = new XmlDocument();
+            document.LoadXml(path);
+            trial.TrialNumberInSzenario = trialNumber;
+        }
+        //Mit der trialNumber kann dann der tpTable durchsucht werden (Trialnumber = Index im TpTable)
+
+        private string getTpTableEntry()
+        {
+            XmlNode node = document.SelectSingleNode("//task_protocol/tp_table/text()");
+
+            return "null";
+        }
+        public int getTrialTargetNumber()
+        {
+
+            return 0;
+        }
+    }
+}
